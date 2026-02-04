0368fe0b0372544de54de8154dc4aa2a132d708f
kanbarayuta <yuta20230617@gmail.com>
2026-02-04 23:48:58 +0900

    Add files via upload

---
+++ 性格診断アプリ/DiagnosticAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiagnosticAppApp.swift
+//  DiagnosticApp
+//
+//  Created by 菅原佑太 on 2026/01/02.
+//
+
+import SwiftUI
+
+@main
+struct DiagnosticAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
